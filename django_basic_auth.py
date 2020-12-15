eb9fe797bee400ad31b177b655e99f5e0db33526
Václav Hůla <vaclav.hula@nubium.cz>
2020-12-15 22:22:28 +0100

    remove fallback

--- django_basic_auth.py
+++ django_basic_auth.py
@@ -78,9 +78,7 @@ def logged_in_or_basicauth(realm = ""):
     def view_decorator(func):
         def wrapper(request, *args, **kwargs):
             return view_or_basicauth(func, request,
-                                     (lambda u: u.is_authenticated())
-                                        if VERSION[0] < 2
-                                        else (lambda u:u.is_authenticated),
+                                     lambda u:u.is_authenticated,
                                      realm, *args, **kwargs)
         return wrapper
     return view_decorator
